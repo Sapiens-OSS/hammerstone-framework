e881b453981e92ddf475f0f72e3f43e223b422bb
nmattela <nmattela@infogroep.be>
2022-08-14 21:53:21 +0200

    For some reason using double mj:warning does not work...

--- scripts/common/resource.lua
+++ scripts/common/resource.lua
@@ -38,6 +38,43 @@ function mod:onload(resource)
 
 		return index
 	end
+
+	-- From the source code. It's a local function so copy/paste is the only way to use it :/
+	local function createGroupHashesForBuiltInTypes()
+		local validGroupTypes = typeMaps:createValidTypesArray("resourceGroup", resource.groups)
+		for i,groupType in ipairs(validGroupTypes) do
+			for j, resourceTypeIndex in ipairs(groupType.resourceTypes) do
+				groupType.containsTypesHash[resourceTypeIndex] = true
+			end
+		end
+	end
+
+	function resource:addResourceGroup(key, objectType)
+		--- Allows adding a resource group.
+		--- @param key: The key to add, such as 'cake'
+		--- @param objectType: The object to add, containing all fields.
+
+		local typeIndexMap = typeMaps.types.resourceGroups -- Created automatically in resource.lua
+
+		local index = typeIndexMap[key]
+		if not index then
+			mj:error("Attempt to add resource group type that isn't in typeIndexMap:", key)
+		else
+			if resource.groups[key] then
+				mj:warning("Overwriting resource group type:", key)
+				mj:log(debug.traceback())
+			end
+	
+			objectType.key = key
+			objectType.index = index
+			typeMaps:insert("resourceGroup", resource.groups, objectType)
+
+			-- Recache the type maps
+			createGroupHashesForBuiltInTypes()
+		end
+
+		return index
+	end
 end
 
 return mod
