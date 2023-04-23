8002a541b1d4f121af46e70222c53859cf580455
SirLich <lrkoehler77@gmail.com>
2023-04-23 16:04:47 +0200

    Release 1.0.0

--- modInfo.lua
+++ modInfo.lua
@@ -1,7 +1,7 @@
 local modInfo = {
     name = "Hammerstone Framework",
-    description = "Hammerstone is a modding API and library for a variety of mods. It often needs to be installed alongside many mods, as they depend on many functions this mod provides. ",
-    version = "0.0.5",
+    description = "Hammerstone is modding API/Library. It is a required dependency for many mods on the workshop.",
+    version = "1.0.0",
     type = "world",
     preview = "assets/hammerstone.png",
     developer = "SirLich & Sapiens Modding Community",
